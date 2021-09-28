207afb9bdce02d5b18210f3b2323930ca24b0ef0
nurgitlab <nurbek.math@gmail.com>
2021-09-28 23:21:00 +0300

    delay learned

--- ecmascript/8_promises.js
+++ ecmascript/8_promises.js
@@ -8,11 +8,11 @@ const delay = ms => new Promise(((resolve, reject) => {
   setTimeout(() => reject(`Done ${ms}`), ms);
 }));
 
-// delay(1000)
-//     .then(data => (delay(500))
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err))
-//     .finally(()=>console.log("finally")))
+delay(1000)
+    .then(data => (delay(500))
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+    .finally(()=>console.log("finally")))
 
 async function asyncDelay() {
   try {
