83a0b7f8ce6c4423fec950e28c3a39a691750886
nurgitlab <nurbek.math@gmail.com>
2021-09-30 22:10:10 +0300

    promises repeated

---
+++ problems from interview/11_typesOfFalsy.js
@@ -0,0 +1,12 @@
+// console.log(false)
+// console.log(!false)
+//
+// console.log(0)
+// console.log(!0)
+// console.log(!!0)
+
+console.log("")
+console.log(!"")
+console.log(!!"")
+
+//In JS 7 types of falses
