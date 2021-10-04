da1a5a262327384efb21731c0988ac4a446b9da4
nurgitlab <nurbek.math@gmail.com>
2021-10-04 22:03:02 +0300

    useeffect hook repeated

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,28 +1,44 @@
 import React, { useState } from "react";
+import List from "./List";
 
+export const mambaContext = React.createContext();
 
 function App() {
-  const [numbers, setNumbers] = React.useState([5, 5]);
+  const seshen = (val) => {
+    console.log(val+" mambet")
+  }
+  
+  const [showList, setShowList] = useState(true)
+  const [shymkentMode, setShymkentMode] = useState("Nurbek")
+  // const dismountComponent = () => setShowList(!showList)
+  const dismountComponent = () => {
+    return setShowList(!showList)
+  }
+  const changeState = () => {
+    setShymkentMode("ABC")
+  }
 
-  let addNumber = () => {
-    const randomNumber = Math.round(Math.random()*100)
-    setNumbers([...numbers, randomNumber])
-    console.log(numbers)
-  };
+  const [answer, setNumbers] = useState(228);
+  const minusOne = () => {
+    setNumbers(answer-1)
+  }
 
   return (
-    <div className="App">
-      <ul>
-        {
-          numbers.map((number, index) => {
-            return (
-              <li key={index}>{number}</li>
-            );
-          })
-        }
-      </ul>
-      <button onClick={addNumber}>New number</button>
-    </div>
+    <mambaContext.Provider value={{
+      value: shymkentMode,
+      setter: setShymkentMode,
+    }}>
+      <div className="App">
+        <button onClick={dismountComponent}>Dismount</button>
+        <button onClick={changeState}>Setter</button>
+        {/*<button onClick={plusOne}>PLUS</button>*/}
+        <button onClick={minusOne}>MINUS</button>
+        {showList && <List />}
+        <div>
+          {answer}
+        </div>
+      </div>
+    </mambaContext.Provider>
   );
 }
 
