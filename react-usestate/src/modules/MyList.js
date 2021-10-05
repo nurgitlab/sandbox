a2ba357ed1dcb83384814b046bc071f57819ba4f
nurgitlab <nurbek.math@gmail.com>
2021-10-05 20:58:38 +0300

    myList app repeated

--- react-usestate/src/modules/MyList.js
+++ react-usestate/src/modules/MyList.js
@@ -1,9 +1,41 @@
 import React from "react"
 
 const MyList = () => {
+  const [words,changeWords] = React.useState(["Lion","Puma"])
+
+  const [inputWord,change] = React.useState("")
+
+  const addWord = () =>{
+    changeWords([...words,inputWord])
+    change("")
+  }
+
+  const inputChange = (e) => {
+    change(e)
+  }
+
+  const deleteWord = () => {
+    words.splice(-1,1)
+    changeWords([...words])
+  }
+
   return(
     <div>
-
+      <input
+        value={inputWord}
+        onChange={e => {inputChange(e.target.value)}}
+      />
+      <button onClick={addWord}>PUSH ELEMENT IN ARRAY</button>
+      <button onClick={deleteWord}>DELETE LAST</button>
+      <ul>
+        {words.map((word,index)=>{
+          return(
+              <li key={index}>
+                {word}
+              </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
