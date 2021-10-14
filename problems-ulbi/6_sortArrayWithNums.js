b53eba5613e46ada0474e173e0ee6cb336813db3
nurgitlab <nurbek.math@gmail.com>
2021-10-14 17:22:20 +0300

    array sort checked

---
+++ problems-ulbi/6_sortArrayWithNums.js
@@ -0,0 +1,16 @@
+let input = [1, 228, 28, 2, 34, 435, 47, 423];
+
+// const answer = input.sort((a, b) => {
+//   if (a - b > 0) {
+//     return 1;
+//   } else {
+//     return -1;
+//   }
+// });
+//
+// console.log(answer)
+
+let answer = input.forEach(element=>console.log(element))
+
+console.log(answer)
+console.log(input)
