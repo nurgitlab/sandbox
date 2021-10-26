4443a82d00e5477ece51afd5a82811c4f5f4247d
nurgitlab <nurbek.math@gmail.com>
2021-10-26 03:07:36 +0300

    clousers repeated

---
+++ problems-min/5_let_const.js
@@ -0,0 +1,25 @@
+//ES6+
+//var -> let/const
+//
+// let a = "variable a";
+// let b = "variable b";
+//
+// {
+//   a = "New Variable A";
+//   let b = "New B";
+//   console.log(b);
+// }
+//
+// console.log(b);
+// console.log(a);
+
+// const PORT = [228, 229, 230]
+// PORT[2] = 1000
+// console.log(PORT)
+
+const obj = {};
+obj.name = "Boni";
+obj.age = 22;
+console.log(obj);
+delete obj.name;
+console.log(obj);
---
+++ problems-min/6_clousers.js
@@ -0,0 +1,39 @@
+// //Clousers
+// function sum(a) {
+//   return function lock(b) {
+//     return a + b;
+//   };
+// }
+//
+// let firstItem = sum(10);
+// console.log(firstItem);
+//
+// let totalSum = firstItem(20);
+// console.log(totalSum);
+
+// function createFrameworkManager() {
+//   const fw = ["Angular", "Vue", "React"]
+//   return {
+//     print: function () {
+//       console.log(fw)
+//     },
+//     add: function (framework) {
+//       fw.push(framework)
+//     }
+//   }
+// }
+// const manager = createFrameworkManager()
+// manager.print()
+// manager.add("Hi")
+// manager.print()
+
+
+const someNumbers = [1, 2, 3, 4, 5, 6, 7];
+
+for (var i = 0; i < someNumbers.length; i++) {
+  (function (j) {
+    setTimeout(function () {
+      console.log(someNumbers[j])
+    },2000)
+  })(i)
+}
