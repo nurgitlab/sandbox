7e2719973236d97011f30f2c21456cfd9523d99c
nurgitlab <nurbek.math@gmail.com>
2021-11-05 23:06:12 +0300

    hello world on react-native

---
+++ react-native-app/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>HELLO WORLD</Text>
+      <Text>This is Shym, op, op!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
